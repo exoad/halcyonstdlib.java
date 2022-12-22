c64de6589ea9c992d9620142e9d5292e37da7727
Jack Meng <jackmeng0814@gmail.com>
2022-12-21 19:51:30 -0500

    first commit

---
+++ pkg/main/java/com/jackmeng/halcyon/Test_StdLib.java
@@ -0,0 +1,7 @@
+package com.jackmeng.halcyon;
+
+public class Test_StdLib {
+    public static void main(String... args) {
+        System.out.println("[StdLib] Hello World!");
+    }
+}
---
+++ pkg/main/java/com/jackmeng/halcyon/package-info.java
@@ -0,0 +1,1 @@
+package com.jackmeng.halcyon;
